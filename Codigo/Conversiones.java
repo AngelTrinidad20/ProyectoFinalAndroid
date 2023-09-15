bcfeec0160b0fda0976979536574adbe5ea7d21b
AngelTrinidad20 <73487635+AngelTrinidad20@users.noreply.github.com>
2023-09-15 16:08:45 -0600

    Add files via upload

---
+++ Codigo/Conversiones.java
@@ -0,0 +1,14 @@
+package com.example.appcalcul002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Conversiones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversiones);
+    }
+}
